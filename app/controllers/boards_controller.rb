dcef48788d938179a4b771d400b4d0f15d0c2c80
Theo Chatonnet <theo.chatonnet@gmail.com>
2023-08-21 17:21:46 +0200

    Doing the new and create actions

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,27 @@
+class BoardsController < ApplicationController
+  def new
+    @user = User.find(params[:user_id])
+    @board = Board.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @board = Board.new(board_params)
+    @board.user = @board
+    if @@board.save
+      redirect_to user_path(@user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def board_params
+    params.require(:board).permit(:name, :description, :board_type, :price, photos: [])
+  end
+end
