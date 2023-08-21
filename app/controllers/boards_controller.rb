c02a539d203af61d1f9d96ce6f95a16dda967b49
Pablo Preault <pablo.preault@kedgebs.com>
2023-08-21 17:24:35 +0200

    index + cards design

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,5 @@
+class BoardsController < ApplicationController
+  def index
+    @boards = Board.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "boards#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :boards, only: [:index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
