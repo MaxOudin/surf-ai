b65f819f291132dcd2ffa1e93f058b4224ddabb7
Maxime Oudin <oudinmaxime@gmail.com>
2023-08-21 16:55:53 +0200

    show view for boards done -design to be improved

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,11 +1,5 @@
 class BoardsController < ApplicationController
-  before_action :set_board, only: %i[show]
   def show
-  end
-
-  private
-
-  def set_board
-    params.require(:boards).permit(:name, :board_type, :description, :price)
+    @board = Board.find(params[:id])
   end
 end
