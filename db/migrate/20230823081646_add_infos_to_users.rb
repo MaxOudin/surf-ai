cf59b5190a5d6e2de16ba01fa96890e618db9872
Maxime Oudin <oudinmaxime@gmail.com>
2023-08-23 10:23:19 +0200

    table users with names and locations

---
+++ db/migrate/20230823081646_add_infos_to_users.rb
@@ -0,0 +1,7 @@
+class AddInfosToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :location, :string
+  end
+end
