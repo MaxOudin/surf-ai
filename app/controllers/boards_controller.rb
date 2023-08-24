6850453daa19ee095a60bf5b8aa2c830da00feaa
Pablo Preault <pablo.preault@kedgebs.com>
2023-08-24 10:58:31 +0200

    generate stimulus map

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,6 +3,12 @@ class BoardsController < ApplicationController
   before_action :authenticate_user!
   def index
     @boards = Board.all
+    @markers = @users.geocoded.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude
+      }
+    end
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ApplicationRecord
   # bookings -> bookings ou user_id == current_user.id
   has_many :bookings, dependent: :destroy
   validates :first_name, :last_name, :location, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
