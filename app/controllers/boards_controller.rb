316ac96d94e2dfbe8b912f1869b151d49e13e26f
Pablo Preault <pablo.preault@kedgebs.com>
2023-08-24 12:46:31 +0200

    mapbox added

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,6 +3,7 @@ class BoardsController < ApplicationController
   before_action :authenticate_user!
   def index
     @boards = Board.all
+    @users = User.joins(:boards).distinct
     @markers = @users.geocoded.map do |user|
       {
         lat: user.latitude,
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
   validates :first_name, :last_name, :location, presence: true
 
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
