d710fc3c570146c142ab08cadc075bd8dcc771f8
Maxime Oudin <oudinmaxime@gmail.com>
2023-08-24 13:14:27 +0200

    default rate ok

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_24_090500) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "average_rating"
+    t.float "average_rating", default: 0.0
     t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
