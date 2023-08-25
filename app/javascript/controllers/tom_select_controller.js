f8fd579961f0097fb41dee39dea053af545dd6b1
Pablo Preault <pablo.preault@kedgebs.com>
2023-08-25 13:10:34 +0200

    form-done

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -6,6 +6,7 @@ export default class extends Controller {
   static values = { options: Object }
 
   connect() {
+    this.element.classList.remove("form-select")
     const options = {
       create: true,
       sortField: {
